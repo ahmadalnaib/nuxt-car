ff8b0a34430ca9e7ef95d845c4b9d9b7d29d4650
ahmadalanib <alnaib888@gmail.com>
2024-11-08 10:52:11 +0100

    finish the ui

---
+++ components/CarAttr.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="mr-10 mt-5 border-b pb-5">
+    <div class="flex text-lg mt-2">
+      <p class="rounded text-time-800 mr-3">✓</p>
+      <p>Leather Interio</p>
+    </div>
+    <div class="flex text-lg mt-2">
+      <p class="rounded text-time-800 mr-3">✓</p>
+      <p>Leather Interio</p>
+    </div>
+
+    <div class="mt-5">
+      <p class="mb-10">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
+        cupiditate!
+      </p>
+    </div>
+
+    <div class="mt-10">
+      <div class="flex w-[600] justify-between">
+          <input type="text" class="border p-1" placeholder="Name" />
+          <input type="text" class="border p-1" placeholder="E-mail" />
+          <input type="text" class="border p-1" placeholder="Phone" />
+        </div>
+        <div class="flex mt-4 w-[600]">
+          <textarea class="border p-1 w-full" placeholder="Message"></textarea>
+        </div>
+        <button class="b-green-500 text-white px-10 py-3 rounded mt-4">
+          Send
+        </button>
+      </div>
+    </div>
+  
+</template>
---
+++ pages/car/[id].vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-1"
+  >
+    <div class="mt-10">
+      <img class="w-full" src="../../public/img/bg.webp" alt="" />
+      <h1 class="mt-10 text-4xl">Jimny</h1>
+      <div
+        class="text-slate-500 flex text-lg mt-3 border-b pb-5 justify-between"
+      >
+        <div class="flex">
+          <p class="mr-2">5 seats</p>
+          <p class="mr-2">|</p>
+          <p class="mr-2">69,444 KM</p>
+        </div>
+        <div class="font-bold text-2xl">22,000</div>
+      </div>
+    </div>
+    <CarAttr />
+  </div>
+</template>
